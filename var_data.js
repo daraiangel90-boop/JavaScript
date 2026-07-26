b3b18b0bf0992f4c0ba72de77c6ae6e7b38d5ce2
Anzal <daraiangel90@gmail.com>
2026-07-26 19:14:05 +0545

    Variables and data types

---
+++ var_data.js
@@ -0,0 +1,42 @@
+const info = {                     //bio
+
+    fullName : "Angel Darai",
+    age : 19,
+    favColor:"Red and Yellow",
+    isStudent: true,
+}
+
+console.log(info);
+
+//=======================================================================
+
+let num1 = 10;                    //calculator
+let num2 = 2;
+
+let sum = num1 + num2;
+let diff = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
+
+console.log('The sum is' +" "+ sum + "."); 
+console.log(diff);
+console.log(mul);
+console.log(div);
+
+//===========================================================================
+
+let newName = "Angel";              //concatenating string
+let oldName = "Darai";
+
+let allName = newName +" "+ oldName;
+
+console.log(allName);
+
+//==========================================================================
+
+let favFood = "Momos";
+let favGame = "Indie";
+let favPlayer = "Lionel Andres Messi";
+let favLanguage = "JavaScript";
+
+console.log("My favourite food is:"+ favFood+"."+" " +"My favourite video game is:"+ favGame+"."+" "+ "My favourite football player is:"+favPlayer+"."+" "+"My favourite programming language is:"+favLanguage+".");
