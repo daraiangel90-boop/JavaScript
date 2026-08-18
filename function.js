5de13e6767e28ca5ac5e4afb7bd1bfb6f1e2ff65
Anzal <daraiangel90@gmail.com>
2026-08-18 21:37:58 +0545

    Function Updated

--- function.js
+++ function.js
@@ -1,6 +1,22 @@
+/*
 let str = prompt("Enter a string:");  //counting vowels in a string
 
 function countVowels(str){
+    let count = 0;
+    for(const char of str){
+        if(char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u'){
+            count++;
+        }
+    }
+    console.log(count);
+}
+
+countVowels(str);
+*/
+
+let str = prompt("Enter a string:");  //counting vowels in a string using arrow function
+
+const countVowels = (str) => {
     let count = 0;
     for(const char of str){
         if(char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u'){
